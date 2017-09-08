379bff532b35577bb125205d13c3985d2eda41b4
Dustin Tran <dustinviettran@gmail.com>
2017-09-08 10:19:26 -0700

    add class memberships to karate.py

--- observations/karate.py
+++ observations/karate.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import numpy as np
 import os
 
 from observations.util import maybe_download_and_extract
@@ -10,10 +11,10 @@ from observations.util import maybe_download_and_extract
 def karate(path):
   """Load Zachary's Karate Club (Zachary, 1977). It is a social
   network of friendships between 34 members of a karate club at a US
-  university from 1970 to 1972. During the study a conflict led the
-  club to split into two. Half of the members formed a new club around
-  the instructor; members from the other part found a new instructor
-  or gave up karate.
+  university from 1970 to 1972. During the study a conflict between
+  instructor 'Mr. Hi' and administrator 'Officer' led the club to
+  split into two. Half of the members formed a new club around 'Mr.
+  Hi'; other members found a new instructor or quit karate.
 
   Args:
     path: str.
@@ -21,8 +22,9 @@ def karate(path):
       be downloaded and extracted there. Filename is `karate.gml`.
 
   Returns:
-    Adjacency matrix as a np.darray `x_train` with 34 rows and 34
-    columns.
+    Tuple of adjacency matrix as a np.darray `x_train` with 34 rows
+    and 34 columns and np.darray `y_train` of class memberships (0 for
+    'Mr.Hi' and 1 for 'Officer').
   """
   import networkx as nx
   path = os.path.expanduser(path)
@@ -33,4 +35,7 @@ def karate(path):
 
   x_train = nx.read_gml(os.path.join(path, filename))
   x_train = nx.to_numpy_matrix(x_train).astype(int)
-  return x_train
+  labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 16, 17, 19, 21]
+  y_train = np.array([0 if i in labels else 1
+                      for i in range(x_train.shape[0])], dtype=np.int)
+  return x_train, y_train
