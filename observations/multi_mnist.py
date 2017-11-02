1638dab1c3e9085870efb170c06b693cd0a895ba
Dustin Tran <dustinviettran@gmail.com>
2017-11-01 18:21:20 -0700

    minor revision

--- observations/multi_mnist.py
+++ observations/multi_mnist.py
@@ -30,7 +30,7 @@ def multi_mnist(path, max_digits=2, canvas_size=50, seed=42):
   Returns:
     Tuple of (np.ndarray of dtype uint8, list)
     `(x_train, y_train), (x_test, y_test)`. Each element in the y's is a
-    list of labels, one label for each digit in the image.
+    np.ndarray of labels, one label for each digit in the image.
   """
   from scipy.misc import imresize
   def _sample_one(canvas_size, x_data, y_data):
@@ -39,9 +39,8 @@ def multi_mnist(path, max_digits=2, canvas_size=50, seed=42):
     label = y_data[i]
     scale = 0.1 * np.random.randn() + 1.3
     resized = imresize(digit, 1.0 / scale)
-    w = resized.shape[0]
-    assert w == resized.shape[1]
-    padding = canvas_size - w
+    width = resized.shape[0]
+    padding = canvas_size - width
     pad_l = np.random.randint(0, padding)
     pad_r = np.random.randint(0, padding)
     pad_width = ((pad_l, padding - pad_l), (pad_r, padding - pad_r))
@@ -54,6 +53,7 @@ def multi_mnist(path, max_digits=2, canvas_size=50, seed=42):
       positioned_digit, label = _sample_one(canvas_size, x_data, y_data)
       canvas += positioned_digit
       labels.append(label)
+    labels = np.array(labels, dtype=np.uint8)
     if np.max(canvas) > 255:  # crude check for overlapping digits
       return _sample_multi(num_digits, canvas_size, x_data, y_data)
     else:
@@ -62,16 +62,17 @@ def multi_mnist(path, max_digits=2, canvas_size=50, seed=42):
     x = []
     y = []
     data_size = x_data.shape[0]
+    data_num_digits = np.random.randint(max_digits + 1, size=data_size)
     x_data = np.reshape(x_data, [data_size, 28, 28])
-    for _ in range(data_size):
-      num_digits = np.random.randint(max_digits + 1)
+    for num_digits in data_num_digits:
       canvas, labels = _sample_multi(num_digits, canvas_size, x_data, y_data)
       x.append(canvas)
       y.append(labels)
     x = np.array(x, dtype=np.uint8)
     return x, y
   path = os.path.expanduser(path)
-  cache_filename = 'multi_mnist_{}_{}_{}.npz'.format(max_digits, canvas_size, seed)
+  cache_filename = 'multi_mnist_{}_{}_{}.npz'.format(
+      max_digits, canvas_size, seed)
   if os.path.exists(os.path.join(path, cache_filename)):
     data = np.load(os.path.join(path, cache_filename))
     return (data['x_train'], data['y_train']), (data['x_test'], data['y_test'])
